ebe0d6c66860b339a5623b97c4777812b18d2909
rygorborodoolin <borodilo2@gmail.com>
2023-01-26 23:32:48 +0200

    More toys for cleanup util

--- UnityProject/Assets/Scripts/Core/Lifecycle/CleanupUtil.cs
+++ UnityProject/Assets/Scripts/Core/Lifecycle/CleanupUtil.cs
@@ -4,8 +4,74 @@ using UnityEngine;
 using System;
 public static class CleanupUtil
 {
+
+	public static int RidListOfSoonToBeDeadElements<T>(IList<T> list_in_question, Func<T, UnityEngine.MonoBehaviour> target_extractor)
+	{
+		if (list_in_question == null)
+		{
+			return -1;
+		}
+
+		List<T> survivor_list = new List<T>();
+
+		for (int i = 0, max = list_in_question.Count; i < max; i++)
+		{
+			MonoBehaviour target = target_extractor(list_in_question[i]);
+
+			if (list_in_question[i] != null && target.gameObject.scene.buildIndex == -1)
+			{
+				survivor_list.Add(list_in_question[i]);
+			}
+		}
+
+		int res = list_in_question.Count - survivor_list.Count;
+		list_in_question.Clear();
+
+		for (int i = 0, max = survivor_list.Count; i < max; i++)
+		{
+			list_in_question.Add(survivor_list[i]);
+		}
+
+		return res;
+	}
+
+	public static int RidListOfDeadElements<T>(IList<T> list_in_question, Func<T, UnityEngine.MonoBehaviour> target_extractor)
+	{
+		if (list_in_question == null)
+		{
+			return -1;
+		}
+
+		List<T> survivor_list = new List<T>();
+
+		for (int i = 0, max = list_in_question.Count; i < max; i++)
+		{
+			MonoBehaviour target = target_extractor(list_in_question[i]);
+
+			if (list_in_question[i] != null && target == null)
+			{
+				survivor_list.Add(list_in_question[i]);
+			}
+		}
+
+		int res = list_in_question.Count - survivor_list.Count;
+		list_in_question.Clear();
+
+		for (int i = 0, max = survivor_list.Count; i < max; i++)
+		{
+			list_in_question.Add(survivor_list[i]);
+		}
+
+		return res;
+	}
+
 	public static int RidListOfDeadElements(IList<Action> list_in_question)
-	{ 
+	{
+		if (list_in_question == null)
+		{
+			return -1;
+		}
+
 		List<Action> survivor_list = new List<Action>();
 
 		for (int i = 0, max = list_in_question.Count; i < max; i++)
@@ -29,6 +95,10 @@ public static class CleanupUtil
 
 	public static int RidListOfDeadElements<T>(IList<T> list_in_question) where T: MonoBehaviour
 	{
+		if (list_in_question == null)
+		{
+			return -1;
+		}
 		List<T> survivor_list = new List<T>();
 		
 		for (int i = 0, max = list_in_question.Count; i < max; i++)
@@ -52,6 +122,10 @@ public static class CleanupUtil
 
 	public static int RidDictionaryOfDeadElements<TKey, TValue>(IDictionary<TKey, TValue> dict_in_question)
 	{
+		if (list_in_question == null)
+		{
+			return -1;
+		}
 		List<KeyValuePair<TKey, TValue>> survivor_list = new List<KeyValuePair<TKey, TValue>>();
 		
 		foreach(var a in dict_in_question)
@@ -83,5 +157,6 @@ public static class CleanupUtil
 	{
 		ComponentManager.ObjectToPhysics.Clear();
 		Spawn.Clean();
+		UpdateManager.Instance.Clear();
 	}
 }
